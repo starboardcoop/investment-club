d39a69a4b91c094328ec4d207617ef1ca223ead0
Dillon Fagan <dillonfagan@pm.me>
2021-09-06 12:29:25 -0400

    Use static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte'
 	},
 
